58b016cff73674f46dc0cb437979f1eb75a4231c
CowCatt <162095280+CowCatt@users.noreply.github.com>
2025-05-15 15:57:07 +0700

    Pertemuan 11

---
+++ resources/views/pertama.blade.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Bantu Adaptasi Mahasiswa Baru, ITS Gelar Mentoring Agama
+            Islam</title>
+
+            <link rel="stylesheet" href="mycss1.css">
+            <style>
+            </style>
+    </head>
+    <body>
+<h2><span style="color:blue;">Adaptasi Mahasiswa Baru, ITS Gelar Mentoring Agama Islam</span></h2>
+<img class="roundedimage" src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2025/02/IMG_20250223_084851-768x432.jpg" width="500" class="roundedimage">
+        <p class="formatheader">Kampus ITS, ITS News — Institut Teknologi Sepuluh Nopember (ITS)
+            kembali menggelar Mentoring Agama Islam Semester Genap 2024/2025
+            dengan berbagai rangkaian kegiatan. Kegiatan ini bertujuan membentuk
+            karakter mahasiswa baru (maba) muslim berbasis nilai-nilai Islam.
+            Acara yang resmi dibuka dalam kuliah umum ini berlangsung di Masjid+
+            Manarul Ilmi ITS, Sabtu (22/02).</p>
+    </body>
+</html>
